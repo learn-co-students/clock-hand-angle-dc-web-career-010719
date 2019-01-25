ac4adf4d90d932201ea5abe117b7db15bebde033
github username <github email address>
2019-01-25 09:43:45 -0500

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,19 @@
+
 def clock_angle(time)
     # code your solution here
+    minute = time.split(':')[1].to_i * 6
+    hour = time.split(':')[0].to_i * 30
+
+    a = (hour - minute).abs
+    if a > 180
+      360 - a
+    else
+      a
+    end
+
 end
+
+puts clock_angle('12:58')
+
+
+#could add potential rewards you 'could' get for customer view rewards
